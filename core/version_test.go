497055466af4d2066d0d7ad2405411e2bef5c54b
Ren0503 <illuminati0440@gmail.com>
2024-09-26 18:28:20 +0700

    enhance: testing flow

---
+++ core/version_test.go
@@ -0,0 +1,196 @@
+package core
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type Response struct {
+	Data interface{} `json:"data"`
+}
+
+func AppModule() ModuleParam {
+	appController1 := func(module *DynamicModule) *DynamicController {
+		ctrl := module.NewController("test").Version("1")
+
+		ctrl.Get("/", func(ctx Ctx) {
+			ctx.JSON(Map{
+				"data": "1",
+			})
+		})
+		return ctrl
+	}
+
+	appController2 := func(module *DynamicModule) *DynamicController {
+		ctrl := module.NewController("test").Version("2")
+
+		ctrl.Get("/", func(ctx Ctx) {
+			ctx.JSON(Map{
+				"data": "2",
+			})
+		})
+		return ctrl
+	}
+
+	module := func() *DynamicModule {
+		appModule := NewModule(NewModuleOptions{
+			Controllers: []Controller{appController1, appController2},
+		})
+
+		return appModule
+	}
+
+	return module
+}
+
+func Test_VersionURI(t *testing.T) {
+	app := CreateFactory(AppModule(), "api")
+	app.EnableVersioning(VersionOptions{
+		Type: URIVersion,
+	})
+
+	testServer := httptest.NewServer(app.prepareBeforeListen())
+	defer testServer.Close()
+	testClient := testServer.Client()
+
+	resp, err := testClient.Get(testServer.URL + "/api/test/v1")
+	require.Nil(t, err)
+	require.Equal(t, resp.StatusCode, http.StatusOK)
+
+	data, err := io.ReadAll(resp.Body)
+	require.Nil(t, err)
+
+	var res Response
+	json.Unmarshal(data, &res)
+	require.Equal(t, res.Data, "1")
+
+	resp2, err := testClient.Get(testServer.URL + "/api/test/v2")
+	require.Nil(t, err)
+	require.Equal(t, resp2.StatusCode, http.StatusOK)
+
+	data2, err := io.ReadAll(resp2.Body)
+	require.Nil(t, err)
+
+	var res2 Response
+	json.Unmarshal(data2, &res2)
+	require.Equal(t, res2.Data, "2")
+}
+
+func Test_VersionHeader(t *testing.T) {
+	app := CreateFactory(AppModule(), "api")
+	app.EnableVersioning(VersionOptions{
+		Type:   HeaderVersion,
+		Header: "X-Version",
+	})
+
+	testServer := httptest.NewServer(app.prepareBeforeListen())
+	defer testServer.Close()
+	testClient := testServer.Client()
+
+	req, err := http.NewRequest("GET", testServer.URL+"/api/test/", nil)
+	require.Nil(t, err)
+	req.Header.Set("X-Version", "1")
+
+	resp, err := testClient.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, resp.StatusCode, http.StatusOK)
+
+	data, err := io.ReadAll(resp.Body)
+	require.Nil(t, err)
+
+	var res Response
+	json.Unmarshal(data, &res)
+	require.Equal(t, res.Data, "1")
+
+	req.Header.Set("X-Version", "2")
+	resp2, err := testClient.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, resp2.StatusCode, http.StatusOK)
+
+	data2, err := io.ReadAll(resp2.Body)
+	require.Nil(t, err)
+
+	var res2 Response
+	json.Unmarshal(data2, &res2)
+	require.Equal(t, res2.Data, "2")
+}
+
+func Test_VersionMedia(t *testing.T) {
+	app := CreateFactory(AppModule(), "api")
+	app.EnableVersioning(VersionOptions{
+		Type: MediaTypeVersion,
+		Key:  "v=",
+	})
+
+	testServer := httptest.NewServer(app.prepareBeforeListen())
+	defer testServer.Close()
+	testClient := testServer.Client()
+
+	req, err := http.NewRequest("GET", testServer.URL+"/api/test/", nil)
+	require.Nil(t, err)
+	req.Header.Set("Accept", "application/json; v=1")
+
+	resp, err := testClient.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, resp.StatusCode, http.StatusOK)
+
+	data, err := io.ReadAll(resp.Body)
+	require.Nil(t, err)
+
+	var res Response
+	json.Unmarshal(data, &res)
+	require.Equal(t, res.Data, "1")
+
+	req.Header.Set("Accept", "application/json; v=2")
+	resp2, err := testClient.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, resp2.StatusCode, http.StatusOK)
+
+	data2, err := io.ReadAll(resp2.Body)
+	require.Nil(t, err)
+
+	var res2 Response
+	json.Unmarshal(data2, &res2)
+	require.Equal(t, res2.Data, "2")
+}
+
+func Test_VersionCustom(t *testing.T) {
+	app := CreateFactory(AppModule(), "api")
+	app.EnableVersioning(VersionOptions{
+		Type: CustomVersion,
+		Extractor: func(r *http.Request) string {
+			return r.URL.Query().Get("version")
+		},
+	})
+
+	testServer := httptest.NewServer(app.prepareBeforeListen())
+	defer testServer.Close()
+	testClient := testServer.Client()
+
+	resp, err := testClient.Get(testServer.URL + "/api/test?version=1")
+	require.Nil(t, err)
+	require.Equal(t, resp.StatusCode, http.StatusOK)
+
+	data, err := io.ReadAll(resp.Body)
+	require.Nil(t, err)
+
+	var res Response
+	json.Unmarshal(data, &res)
+	require.Equal(t, res.Data, "1")
+
+	resp2, err := testClient.Get(testServer.URL + "/api/test?version=2")
+	require.Nil(t, err)
+	require.Equal(t, resp2.StatusCode, http.StatusOK)
+
+	data2, err := io.ReadAll(resp2.Body)
+	require.Nil(t, err)
+
+	var res2 Response
+	json.Unmarshal(data2, &res2)
+	require.Equal(t, res2.Data, "2")
+}
