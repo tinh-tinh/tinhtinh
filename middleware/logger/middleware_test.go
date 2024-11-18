4b289f30c962e8d4b3c2eb6886162d6944ba49ce
Ren0503 <illuminati0440@gmail.com>
2024-11-18 09:30:47 +0700

    feat: add constants format for logger

--- middleware/logger/middleware_test.go
+++ middleware/logger/middleware_test.go
@@ -76,3 +76,39 @@ func TestMiddleware(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, http.StatusInternalServerError, resp.StatusCode)
 }
+
+func TestDev(t *testing.T) {
+	appController := func(module *core.DynamicModule) *core.DynamicController {
+		ctrl := module.NewController("test")
+
+		ctrl.Get("", func(ctx core.Ctx) error {
+			return ctx.JSON(core.Map{
+				"data": "ok",
+			})
+		})
+
+		return ctrl
+	}
+
+	appModule := func() *core.DynamicModule {
+		return core.NewModule(core.NewModuleOptions{
+			Controllers: []core.Controller{appController},
+		})
+	}
+
+	app := core.CreateFactory(appModule)
+	app.SetGlobalPrefix("/api")
+	app.Use(logger.Handler(logger.MiddlewareOptions{
+		Format: logger.Common,
+		Rotate: true,
+	}))
+
+	testServer := httptest.NewServer(app.PrepareBeforeListen())
+	defer testServer.Close()
+
+	testClient := testServer.Client()
+
+	resp, err := testClient.Get(testServer.URL + "/api/test")
+	require.Nil(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+}
