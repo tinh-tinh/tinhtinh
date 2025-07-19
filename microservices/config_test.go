c03f499cfb4126911f741b1e9058636c0bec068b
Ren0503 <illuminati0440@gmail.com>
2025-07-19 13:59:09 +0700

    feat(microservices): remove option logger when connecT

--- microservices/config_test.go
+++ microservices/config_test.go
@@ -5,14 +5,12 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/tinh-tinh/tinhtinh/microservices"
-	"github.com/tinh-tinh/tinhtinh/v2/middleware/logger"
 )
 
 func Test_Default(t *testing.T) {
 	config := microservices.DefaultConfig()
 	require.NotNil(t, config.Serializer)
 	require.NotNil(t, config.Deserializer)
-	require.NotNil(t, config.Logger)
 	require.NotNil(t, config.ErrorHandler)
 	require.NotNil(t, config.Header)
 
@@ -24,7 +22,6 @@ func Test_Default(t *testing.T) {
 			return nil
 		},
 		ErrorHandler: func(err error) {},
-		Logger:       logger.Create(logger.Options{}),
 		Header:       map[string]string{"abc": "123"},
 	})
 	require.NotEqual(t, config, cfg)
